d6b32e6beff62fc1cb64d027d6ecfb82d0644e48
Simmons, John <JohnSimmons@quickenloans.com>
2016-07-14 16:22:54 -0400

    cl route

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from content import Content
 from requests import get
 import json
 import os
+import datetime
 
 app = Flask(__name__)
 
@@ -86,8 +87,40 @@ def create_routes(app):
 
     @app.route('/cl')
     def cl():
+        from bs4 import BeautifulSoup as bs4
         try:
-            return render_template('cl.html')
+            base_url = 'http://detroit.craigslist.org'
+
+            searches = {
+                'marartz':'http://detroit.craigslist.org/search/ele?query=marantz&sort=date&postedToday=1', 
+                'pioneer': 'http://detroit.craigslist.org/search/ele?postedToday=1&amp;query=pioneer%20receiver&amp;sort=date',
+                'minidisc': 'http://detroit.craigslist.org/search/okl/ele?query=minidisc&sort=date&postedToday=1',
+                'reel to reel': 'http://detroit.craigslist.org/search/ele?query=reel+to+reel&sort=date&postedToday=1'
+            }
+
+            pages = []
+            html_string = '<p>Updated On: {}'.format(datetime.datetime.now().strftime('%c'))
+
+
+            for search, url in searches.iteritems():
+                p = get(url).text
+                pages.append((bs4(p, 'html5lib'), search))
+
+            for page in pages:
+
+                # Check if results exist
+                results = page[0].findAll("p", { "class" : "row" })
+                if results:
+                    for listing in results:
+                        if not listing.find('a').get('href').startswith('//annarbor'):
+                            link = base_url + listing.find('a').get('href')
+                            try:
+                                price = listing.find("span", { "class" : "price" }).text
+                            except:
+                                price = 'No Price'
+                            title = listing.find("span", { "id" : "titletextonly" }).text
+                            html_string += '<p><strong>{3}</strong>: <a href="{0}">{1} - {2}</a></p>'.format(link, title, price, page[1])
+            return render_template('cl.html', html_string=html_string)
         except Exception as e:
             return render_template('500.html', CONTENT_DICT=CONTENT_DICT, error=e)
 
