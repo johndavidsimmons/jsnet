f2cd4c945664b9c02c903acf85f3aa6a594cd7ac
Simmons, John <JohnSimmons@quickenloans.com>
2017-04-11 13:15:07 -0400

    pdf resume

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,25 @@
-from flask import render_template
+from flask import render_template, send_file
 from . import main
 from content import Content
 
 CONTENT_DICT = Content()
 
+
 @main.route('/', methods=['GET'])
 def index():
 	return render_template('index.html', CONTENT_DICT=CONTENT_DICT)
+
 
 @main.route('/resume', methods=['GET'])
 def resume():
 	return render_template('resume.html', CONTENT_DICT=CONTENT_DICT)
 
+
 @main.route('/todo', methods=['GET'])
 def todo():
 	return render_template('todo.html', CONTENT_DICT=CONTENT_DICT)
+
+
+@main.route('/download', methods=['GET'])
+def download():
+	return send_file('static/John_Simmons_Resume.pdf', as_attachment=True)
